1942d5e70213c9993530281eed103637cc171622
LukeKedz <lukekedziora@gmail.com>
2018-09-29 09:41:08 -0400

    ready to go auto with live_scoring

--- live_scoring.rb
+++ live_scoring.rb
@@ -2,7 +2,12 @@ require 'httparty'
 require 'nokogiri'
 require 'rufus-scheduler'
 
-system "echo 'live scoring running!' | mail -s 'Kronos Live Scoring' lukekedziora@gmail.com"
+# TODO: auto-close scoring & finalize week
+# TODO: logging, error emails
+
+def game_date(date)
+  Date.new(date.to_s[0..3].to_i, date.to_s[5..6].to_i, date.to_s[8..9].to_i)
+end
 
 def strip_RZ_from_team_name(team_name)
   # example 'OSURZ'
@@ -23,138 +28,153 @@ def parse_points_from_attribute(attribute)
   attribute.css('span').first.text
 end
 
-active_week = HTTParty.get('https://shepic.herokuapp.com/admin/week_number_for_scripts', :body => { :secret => ARGV[0] })
+# mirroring synchro's setup: is there a game today?
+todays_date      = Date.today
+game_sched_today = false
+
 weekly_game_slate = HTTParty.get('https://shepic.herokuapp.com/admin/active_game_slate', :body => { :secret => ARGV[0] })
-games = []
-weekly_game_slate.each do |game|
-  details = {
-    :id             => game['id'],
-    :game_finished  => game['game_finished'],
-    :away_team      => game['away'],
-    :home_team      => game['home'],
-  }
-  games.push details
-end
+if weekly_game_slate && weekly_game_slate.parsed_response != nil
+  weekly_game_slate.each do |game|
+    if todays_date == game_date(game['date'])
+      game_sched_today = true
+      break
+    end
+  end
 
-scheduler = Rufus::Scheduler.new
-scheduler.every '5m', :first_in => 0 do
-  puts
-  puts '*'*100
-
-  scrape = HTTParty.get("https://sports.yahoo.com/college-football/scoreboard/?confId=1%2C4%2C6%2C7%2C8%2C11%2C71%2C72%2C87%2C90%2C122&schedState=2&dateRange=#{active_week['week']}")
-  live_html_elements = Nokogiri::HTML(scrape).css('#scoreboard-group-2 div ul li').css('div')
-
-  time_remaining, away_team, home_team, away_pts, home_pts = nil
-  live_html_elements.each do |el|
-    # TIME REMAINING
-    if el.attributes['class'] && el.attributes['class'].value == 'Ta(end) Cl(b) Fw(b) '
-      if el.css('ul li').last && el.css('ul li').last.text
-        time_remaining = el.css('ul li').last.text
-      end
+  if game_sched_today
+    active_week = HTTParty.get('https://shepic.herokuapp.com/admin/week_number_for_scripts', :body => { :secret => ARGV[0] })
+    games = []
+    weekly_game_slate.each do |game|
+      details = {
+        :id             => game['id'],
+        :game_finished  => game['game_finished'],
+        :away_team      => game['away'],
+        :home_team      => game['home'],
+      }
+      games.push details
     end
 
-    if time_remaining != nil
-      # TEAMS
-      if el.attributes['class'] && el.attributes['class'].value == 'Fw(b) Fz(14px)'
-        team_name = el.children.children.text
-        team_name = strip_RZ_from_team_name(team_name)
-        team_name = strip_rank_from_team_name(team_name)
+    scheduler = Rufus::Scheduler.new
+    scheduler.every '5m', :first_in => 0 do
+      puts
+      puts '*'*100
 
-        away_team == nil ? away_team = team_name : home_team = team_name
-      end
+      scrape = HTTParty.get("https://sports.yahoo.com/college-football/scoreboard/?confId=1%2C4%2C6%2C7%2C8%2C11%2C71%2C72%2C87%2C90%2C122&schedState=2&dateRange=#{active_week['week']}")
+      live_html_elements = Nokogiri::HTML(scrape).css('#scoreboard-group-2 div ul li').css('div')
 
-      # POINTS
-      if el.attributes['class'] && el.attributes['class'].value == 'Whs(nw) D(tbc) Va(m) Fw(b) Fz(27px)'
-        points = parse_points_from_attribute(l)
-        away_pts == nil ? away_pts = points : home_pts = points
-      end
-    end
+      time_remaining, away_team, home_team, away_pts, home_pts = nil
+      live_html_elements.each do |el|
+        # TIME REMAINING
+        if el.attributes['class'] && el.attributes['class'].value == 'Ta(end) Cl(b) Fw(b) '
+          if el.css('ul li').last && el.css('ul li').last.text
+            time_remaining = el.css('ul li').last.text
+          end
+        end
 
-    if time_remaining != nil &&
-       away_team      != nil &&
-       home_team      != nil &&
-       away_pts       != nil &&
-       home_pts       != nil
+        if time_remaining != nil
+          # TEAMS
+          if el.attributes['class'] && el.attributes['class'].value == 'Fw(b) Fz(14px)'
+            team_name = el.children.children.text
+            team_name = strip_RZ_from_team_name(team_name)
+            team_name = strip_rank_from_team_name(team_name)
 
-      games.each do |g|
-        next if g[:game_finished] == true
+            away_team == nil ? away_team = team_name : home_team = team_name
+          end
 
-        if away_team == g[:away_team] && home_team == g[:home_team]
-          puts
-          puts 'TIME: ' + time_remaining.inspect
-          puts 'AWAY: ' + away_team..ljust(20, ' ') + ' => ' + away_pts.inspect
-          puts 'HOME: ' + home_team..ljust(20, ' ') + ' => ' + home_pts.inspect
+          # POINTS
+          if el.attributes['class'] && el.attributes['class'].value == 'Whs(nw) D(tbc) Va(m) Fw(b) Fz(27px)'
+            points = parse_points_from_attribute(el)
+            away_pts == nil ? away_pts = points : home_pts = points
+          end
+        end
 
-          HTTParty.post('https://shepic.herokuapp.com/admin/update_score', :body => {
-            :secret         => ARGV[0],
-            :id             => g[:id],
-            :game_finished  => false,
-            :time_remaining => time_remaining,
-            :away_pts       => away_pts,
-            :home_pts       => home_pts,
-          })
-        else
-          puts 'NO MATCH! ... AWAY: ' + away_team + '  HOME: ' + home_team
+        if time_remaining != nil &&
+           away_team      != nil &&
+           home_team      != nil &&
+           away_pts       != nil &&
+           home_pts       != nil
+
+          games.each do |g|
+            next if g[:game_finished] == true
+
+            if away_team == g[:away_team] && home_team == g[:home_team]
+              puts
+              puts 'TIME: ' + time_remaining.to_s
+              puts 'AWAY: ' +  away_pts.to_s.ljust(3, ' ') + away_team
+              puts 'HOME: ' +  home_pts.to_s.ljust(3, ' ') + home_team
+
+              HTTParty.post('https://shepic.herokuapp.com/admin/update_score', :body => {
+                :secret         => ARGV[0],
+                :id             => g[:id],
+                :game_finished  => false,
+                :time_remaining => time_remaining,
+                :away_pts       => away_pts,
+                :home_pts       => home_pts,
+              })
+            else
+              # puts 'NO MATCH! ... AWAY: ' + away_team + '  HOME: ' + home_team
+            end
+          end
+
+          time_remaining, away_team, home_team, away_pts, home_pts = nil
         end
       end
 
       time_remaining, away_team, home_team, away_pts, home_pts = nil
-    end
-  end
+      finished_html_elements = Nokogiri::HTML(scrape).css('#scoreboard-group-2 div:nth-child(3) ul li').css('div')
+      finished_html_elements.each_with_index do |el|
+        # TIME REMAINING
+        if el.attributes['class'] && el.attributes['class'].value == 'Ta(end) Cl(b) Fw(b) '
+          time_remaining = el.text
+        end
 
-  time_remaining, away_team, home_team, away_pts, home_pts = nil
-  finished_html_elements = Nokogiri::HTML(scrape).css('#scoreboard-group-2 div:nth-child(3) ul li').css('div')
-  finished_html_elements.each_with_index do |el|
-    # TIME REMAINING
-    if el.attributes['class'] && el.attributes['class'].value == 'Ta(end) Cl(b) Fw(b) '
-      time_remaining = el.text
-    end
+        if time_remaining != nil
+          # TEAMS
+          if el.attributes['class'] && el.attributes['class'].value == 'Fw(b) Fz(14px)'
+            team_name = el.text
+            team_name = strip_RZ_from_team_name(team_name)
+            team_name = strip_rank_from_team_name(team_name)
 
-    if time_remaining != nil
-      # TEAMS
-      if el.attributes['class'] && el.attributes['class'].value == 'Fw(b) Fz(14px)'
-        team_name = el.text
-        team_name = strip_RZ_from_team_name(team_name)
-        team_name = strip_rank_from_team_name(team_name)
+            away_team == nil ? away_team = team_name : home_team = team_name
+          end
 
-        away_team == nil ? away_team = team_name : home_team = team_name
-      end
+          # POINTS
+          if el.attributes['class'] && el.attributes['class'].value == 'Whs(nw) D(tbc) Va(m) Fw(b) Fz(27px)'
+            points = parse_points_from_attribute(el)
+            away_pts == nil ? away_pts = points : home_pts = points
+          end
+        end
 
-      # POINTS
-      if el.attributes['class'] && el.attributes['class'].value == 'Whs(nw) D(tbc) Va(m) Fw(b) Fz(27px)'
-        points = parse_points_from_attribute(l)
-        away_pts == nil ? away_pts = points : home_pts = points
-      end
-    end
+        if time_remaining != nil &&
+           away_team      != nil &&
+           home_team      != nil &&
+           away_pts       != nil &&
+           home_pts       != nil
 
-    if time_remaining != nil &&
-       away_team      != nil &&
-       home_team      != nil &&
-       away_pts       != nil &&
-       home_pts       != nil
+          games.each do |g|
+            next if g[:game_finished] == true
 
-      games.each do |g|
-        next if g[:game_finished] == true
+            if away_team == g[:away_team] && home_team == g[:home_team]
+              g[:game_finished] = (time_remaining == 'Final' || time_remaining == 'Final OT'  ? true : false)
 
-        if away_team == g[:away_team] && home_team == g[:home_team]
-          g[:game_finished] = (time_remaining == 'Final' || time_remaining == 'Final OT'  ? true : false)
+              HTTParty.post('https://shepic.herokuapp.com/admin/update_score', :body => {
+                :secret => ARGV[0],
+                :id => g[:id],
+                :game_finished => g[:game_finished],
+                :time_remaining => time_remaining,
+                :away_pts => away_pts,
+                :home_pts => home_pts,
+              })
+            end
+          end
 
-          HTTParty.post('https://shepic.herokuapp.com/admin/update_score', :body => {
-            :secret => ARGV[0],
-            :id => g[:id],
-            :game_finished => g[:game_finished],
-            :time_remaining => time_remaining,
-            :away_pts => away_pts,
-            :home_pts => home_pts,
-          })
+          time_remaining, away_team, home_team, away_pts, home_pts = nil
         end
       end
 
-      time_remaining, away_team, home_team, away_pts, home_pts = nil
+      if games.map { |g| g[:game_finished] }.all? then exit end
     end
-  end
+    scheduler.join
 
-  # TODO: if all games aren't on same day, will not turn off!
-  if games.map { |g| g[:game_finished] }.all? then exit end
+  end
 end
-scheduler.join
